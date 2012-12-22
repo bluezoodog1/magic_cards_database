82453b75521ab9323dd3340bad48741635f15b36
Justin R. Butera <bluezoodog1@buteradesign.com>
2012-12-22 14:24:45 -0800

    version 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagicCardsDatabase::Application.config.secret_token = 'c910fe0ffe9a9383f30f4b2ac23fa985146b3fae9449571a3111c5f2eb88cfa6d5bac783f345637887b40f3feb3bab169b6b448c34909f1776f0a7df53a763bb'
